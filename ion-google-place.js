7d55b4dbaa515902092b88f3156d9aec0e595b18
thco <perso@thco.fr>
2015-07-22 14:27:02 +0200

    custom directive for smitec

--- ion-google-place.js
+++ ion-google-place.js
@@ -1,5 +1,5 @@
 angular.module('ion-google-place', [])
-    .directive('ionGooglePlace', [
+  .directive('ionGooglePlace', [
         '$ionicTemplateLoader',
         '$ionicBackdrop',
         '$ionicPlatform',
@@ -7,28 +7,42 @@ angular.module('ion-google-place', [])
         '$timeout',
         '$rootScope',
         '$document',
-        function($ionicTemplateLoader, $ionicBackdrop, $ionicPlatform, $q, $timeout, $rootScope, $document) {
-            return {
-                require: '?ngModel',
-                restrict: 'E',
-                template: '<input type="text" readonly="readonly" class="ion-google-place" autocomplete="off">',
-                replace: true,
-                scope: {
-                    ngModel: '=?',
-                    geocodeOptions: '='
-                },
-                link: function(scope, element, attrs, ngModel) {
-                    var unbindBackButtonAction;
+        function ($ionicTemplateLoader, $ionicBackdrop, $ionicPlatform, $q, $timeout, $rootScope, $document) {
+      return {
+        require: '?ngModel',
+        restrict: 'E',
+        template: '<input type="text" readonly="readonly" class="ion-google-place" autocomplete="off">',
+        replace: true,
+        scope: {
+          ngModel: '=?',
+          geocodeOptions: '='
+        },
+        link: function (scope, element, attrs, ngModel) {
+          if (!scope.$root.api) {
+            var api = document.querySelector('google-maps-api');
+            api.addEventListener('api-load', function (e) {
+              link(scope, element, attrs, ngModel, $ionicTemplateLoader, $ionicBackdrop, $ionicPlatform, $q, $timeout, $rootScope, $document);
+            });
+          } else {
+            link(scope, element, attrs, ngModel, $ionicTemplateLoader, $ionicBackdrop, $ionicPlatform, $q, $timeout, $rootScope, $document);
+          }
+        }
+      };
+        }
+    ]);
 
-                    scope.locations = [];
-                    var geocoder = new google.maps.Geocoder();
-                    var searchEventTimeout = undefined;
+var link = function (scope, element, attrs, ngModel, $ionicTemplateLoader, $ionicBackdrop, $ionicPlatform, $q, $timeout, $rootScope, $document) {
+  var unbindBackButtonAction;
 
-                    var POPUP_TPL = [
+  scope.locations = [];
+  var geocoder = new google.maps.Geocoder();
+  var searchEventTimeout = undefined;
+
+  var POPUP_TPL = [
                         '<div class="ion-google-place-container modal">',
                             '<div class="bar bar-header item-input-inset">',
                                 '<label class="item-input-wrapper">',
-                                    '<i class="icon ion-ios7-search placeholder-icon"></i>',
+                                    '<i class="icon ion-search placeholder-icon"></i>',
                                     '<input class="google-place-search" type="search" ng-model="searchQuery" placeholder="' + (attrs.searchPlaceholder || 'Enter an address, place or ZIP code') + '">',
                                 '</label>',
                                 '<button class="button button-clear">',
@@ -45,119 +59,121 @@ angular.module('ion-google-place', [])
                         '</div>'
                     ].join('');
 
-                    var popupPromise = $ionicTemplateLoader.compile({
-                        template: POPUP_TPL,
-                        scope: scope,
-                        appendTo: $document[0].body
-                    });
+  var popupPromise = $ionicTemplateLoader.compile({
+    template: POPUP_TPL,
+    scope: scope,
+    appendTo: $document[0].body
+  });
 
-                    popupPromise.then(function(el){
-                        var searchInputElement = angular.element(el.element.find('input'));
+  popupPromise.then(function (el) {
+    var searchInputElement = angular.element(el.element.find('input'));
 
-                        scope.selectLocation = function(location){
-                            ngModel.$setViewValue(location);
-                            ngModel.$render();
-                            el.element.css('display', 'none');
-                            $ionicBackdrop.release();
+    scope.selectLocation = function (location) {
+      ngModel.$setViewValue(location);
+      ngModel.$render();
+      el.element.css('display', 'none');
+      $ionicBackdrop.release();
 
-                            if (unbindBackButtonAction) {
-                                unbindBackButtonAction();
-                                unbindBackButtonAction = null;
-                            }
-                        };
+      if (unbindBackButtonAction) {
+        unbindBackButtonAction();
+        unbindBackButtonAction = null;
+      }
+    };
 
-                        scope.$watch('searchQuery', function(query){
-                            if (searchEventTimeout) $timeout.cancel(searchEventTimeout);
-                            searchEventTimeout = $timeout(function() {
-                                if(!query) return;
-                                if(query.length < 3);
+    scope.$watch('searchQuery', function (query) {
+      if (searchEventTimeout) $timeout.cancel(searchEventTimeout);
+      searchEventTimeout = $timeout(function () {
+        if (!query) return;
+        if (query.length < 3);
 
-                                var req = scope.geocodeOptions || {};
-                                req.address = query;
-                                geocoder.geocode(req, function(results, status) {
-                                    if (status == google.maps.GeocoderStatus.OK) {
-                                        scope.$apply(function(){
-                                            scope.locations = results;
-                                        });
-                                    } else {
-                                        // @TODO: Figure out what to do when the geocoding fails
-                                    }
-                                });
-                            }, 350); // we're throttling the input by 350ms to be nice to google's API
-                        });
+        var req = scope.geocodeOptions || {};
+        req.address = query;
+        geocoder.geocode(req, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            scope.$apply(function () {
+              scope.locations = results;
+            });
+          } else {
+            // @TODO: Figure out what to do when the geocoding fails
+          }
+        });
+      }, 350); // we're throttling the input by 350ms to be nice to google's API
+    });
 
-                        var onClick = function(e){
-                            e.preventDefault();
-                            e.stopPropagation();
+    var onClick = function (e) {
+      e.preventDefault();
+      e.stopPropagation();
 
-                            $ionicBackdrop.retain();
-                            unbindBackButtonAction = $ionicPlatform.registerBackButtonAction(closeOnBackButton, 250);
+      $ionicBackdrop.retain();
+      unbindBackButtonAction = $ionicPlatform.registerBackButtonAction(closeOnBackButton, 250);
 
-                            el.element.css('display', 'block');
-                            searchInputElement[0].focus();
-                            setTimeout(function(){
-                                searchInputElement[0].focus();
-                            },0);
-                        };
+      el.element.css('display', 'block');
+      searchInputElement[0].focus();
+      setTimeout(function () {
+        searchInputElement[0].focus();
+      }, 0);
+    };
 
-                        var onCancel = function(e){
-                            scope.searchQuery = '';
-                            $ionicBackdrop.release();
-                            el.element.css('display', 'none');
+    var onCancel = function (e) {
+      scope.searchQuery = '';
+      $ionicBackdrop.release();
+      el.element.css('display', 'none');
 
-                            if (unbindBackButtonAction){
-                                unbindBackButtonAction();
-                                unbindBackButtonAction = null;
-                            }
-                        };
+      if (unbindBackButtonAction) {
+        unbindBackButtonAction();
+        unbindBackButtonAction = null;
+      }
+    };
 
-                        closeOnBackButton = function(e){
-                            e.preventDefault();
+    closeOnBackButton = function (e) {
+      e.preventDefault();
 
-                            el.element.css('display', 'none');
-                            $ionicBackdrop.release();
+      el.element.css('display', 'none');
+      $ionicBackdrop.release();
 
-                            if (unbindBackButtonAction){
-                                unbindBackButtonAction();
-                                unbindBackButtonAction = null;
-                            }
-                        }
+      if (unbindBackButtonAction) {
+        unbindBackButtonAction();
+        unbindBackButtonAction = null;
+      }
+    }
 
-                        element.bind('click', onClick);
-                        element.bind('touchend', onClick);
+    element.bind('click', onClick);
+    element.bind('touchend', onClick);
 
-                        el.element.find('button').bind('click', onCancel);
-                    });
+    el.element.find('button').bind('click', onCancel);
+  });
 
-                    if(attrs.placeholder){
-                        element.attr('placeholder', attrs.placeholder);
-                    }
+  if (attrs.placeholder) {
+    element.attr('placeholder', attrs.placeholder);
+  }
 
 
-                    ngModel.$formatters.unshift(function (modelValue) {
-                        if (!modelValue) return '';
-                        return modelValue;
-                    });
+  ngModel.$formatters.unshift(function (modelValue) {
+    if (!modelValue) return '';
+    return modelValue;
+  });
 
-                    ngModel.$parsers.unshift(function (viewValue) {
-                        return viewValue;
-                    });
+  ngModel.$parsers.unshift(function (viewValue) {
+    return viewValue;
+  });
 
-                    ngModel.$render = function(){
-                        if(!ngModel.$viewValue){
-                            element.val('');
-                        } else {
-                            element.val(ngModel.$viewValue.formatted_address || '');
-                        }
-                    };
+  ngModel.$render = function () {
+    if (!ngModel.$viewValue) {
+      element.val('');
+    } else {
+      element.val(ngModel.$viewValue.formatted_address || '');
+    }
 
-                    scope.$on("$destroy", function(){
-                        if (unbindBackButtonAction){
-                            unbindBackButtonAction();
-                            unbindBackButtonAction = null;
-                        }
-                    });
-                }
-            };
-        }
-    ]);
+    scope.$root.$broadcast('smitec.google-place', {
+      model: ngModel,
+      type: attrs.ngModel
+    });
+  };
+
+  scope.$on("$destroy", function () {
+    if (unbindBackButtonAction) {
+      unbindBackButtonAction();
+      unbindBackButtonAction = null;
+    }
+  });
+}
